0fd88c2639a3e4ec99c79992e2ee01b0b7a3be7d
Silberengel <silberengel7@protonmail.com>
2024-12-31 03:47:26 -0800

    Updated about page links.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,9 +7,9 @@
 <div class='w-full flex justify-center'>
   <main class='main-leather flex flex-col space-y-4 max-w-2xl w-full mt-4 mb-4'>
     <Heading tag='h1' class='h-leather mb-2'>About</Heading>
-    <p>Alexandria is a <a href="https://wikistr.com/nkbip-01" class="text-indigo-600 underline">Nostr Knowledge Base (NKB)</a> and a reader for long-form articles.
-      It is produced by <a href="https://wikistr.com/gitcitadel-project" class="text-indigo-600 underline">GitCitadel</a>.</p>
+    <p>Alexandria is an editor and generator for <a href="https://github.com/nostr-protocol/nips/pull/1600" class="text-indigo-600 underline">curated publications</a> and will soon also be a reader for long-form articles and wiki pages.
+      It is produced by the <a href="https://wikistr.com/gitcitadel-project" class="text-indigo-600 underline">GitCitadel project team</a>.</p>
 
-    <p>Please submit support issues on the <a href="https://gitworkshop.dev/r/naddr1qvzqqqrhnypzplfq3m5v3u5r0q9f255fdeyz8nyac6lagssx8zy4wugxjs8ajf7pqy88wumn8ghj7mn0wvhxcmmv9uqq5emfw33kjarpv3jkcs83wav" class="text-indigo-600 underline">GitWorkshop page</a> and follow us on <a href="https://github.com/ShadowySupercode/gitcitadel" class="text-indigo-600 underline">GitHub</a>.</p>
+    <p>Please submit support issues on the <a href="https://gitcitadel.com/r/naddr1qvzqqqrhnypzplfq3m5v3u5r0q9f255fdeyz8nyac6lagssx8zy4wugxjs8ajf7pqy88wumn8ghj7mn0wvhxcmmv9uqq5emfw33kjarpv3jkcs83wav" class="text-indigo-600 underline">project repo page</a> and follow us on <a href="https://github.com/ShadowySupercode/gitcitadel" class="text-indigo-600 underline">GitHub</a> and <a href="https://geyser.fund/project/gitcitadel" class="text-indigo-600 underline">Geyserfund</a>.</p>
   </main>
 </div>
