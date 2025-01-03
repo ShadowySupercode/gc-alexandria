f447876f2512e7f0d94d8a8e3be089930eba58d6
buttercat1791 <mjjurkoic@gmail.com>
2025-01-03 09:21:52 -0600

    Remove unused imports

--- src/lib/ndk.ts
+++ src/lib/ndk.ts
@@ -1,7 +1,5 @@
-import { browser } from '$app/environment';
 import NDK from '@nostr-dev-kit/ndk';
 import { writable, type Writable } from 'svelte/store';
-import { standardRelays } from './consts';
 
 export const ndkInstance: Writable<NDK> = writable();
 
