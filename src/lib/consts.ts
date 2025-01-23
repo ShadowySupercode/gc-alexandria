546bb84ffda35cf67c4dd01bab8814dffb9c0938
Silberengel <silberengel7@protonmail.com>
2025-01-22 22:36:50 -0800

    Remove land from standard relays

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,7 +1,7 @@
 export const wikiKind = 30818;
 export const indexKind = 30040;
 export const zettelKinds = [ 30041 ];
-export const standardRelays = [ "wss://thecitadel.nostr1.com", "wss://nostr.land", "wss://relay.noswhere.com" ];
+export const standardRelays = [ "wss://thecitadel.nostr1.com", "wss://relay.noswhere.com" ];
 
 export enum FeedType {
   StandardRelays,
