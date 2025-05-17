ef1381caba078664f59394f5198aecfb97c279c8
Silberengel <silberengel7@protonmail.com>
2025-05-17 08:38:05 +0200

    Getting started page created, navigation bar updated, added padding to the bottom of publication and an end-of-publication notice, at the bottom, instead of "show more". closes issue#235

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,29 +1,37 @@
 <script lang="ts">
-  import { DarkMode, Navbar, NavLi, NavUl, NavHamburger, NavBrand } from 'flowbite-svelte';
-  import Login from './Login.svelte';
+  import {
+    DarkMode,
+    Navbar,
+    NavLi,
+    NavUl,
+    NavHamburger,
+    NavBrand,
+  } from "flowbite-svelte";
+  import Login from "./Login.svelte";
 
-  let { class: className = '' } = $props();
+  let { class: className = "" } = $props();
 
   let leftMenuOpen = $state(false);
 </script>
 
 <Navbar class={`Navbar navbar-leather ${className}`}>
-  <div class='flex flex-grow justify-between'>
-    <NavBrand href='/'>
+  <div class="flex flex-grow justify-between">
+    <NavBrand href="/">
       <h1>Alexandria</h1>
     </NavBrand>
   </div>
-  <div class='flex md:order-2'>
+  <div class="flex md:order-2">
     <Login />
-    <NavHamburger class='btn-leather' />
+    <NavHamburger class="btn-leather" />
   </div>
-  <NavUl class='ul-leather'>
-    <NavLi href='/new/edit'>Publish</NavLi>
-    <NavLi href='/visualize'>Visualize</NavLi>
-    <NavLi href='/about'>About</NavLi>
-    <NavLi href='/contact'>Contact</NavLi>
-  <NavLi>
-      <DarkMode btnClass='btn-leather p-0'/>
+  <NavUl class="ul-leather">
+    <NavLi href="/new/edit">Publish</NavLi>
+    <NavLi href="/visualize">Visualize</NavLi>
+    <NavLi href="/start">Getting Started</NavLi>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+    <NavLi>
+      <DarkMode btnClass="btn-leather p-0" />
     </NavLi>
   </NavUl>
 </Navbar>
