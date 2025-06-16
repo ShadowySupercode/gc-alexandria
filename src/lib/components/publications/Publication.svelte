31b5c2c3243b3a5d175fafe995b33e5eea23e7a0
buttercat1791 <mjjurkoic@gmail.com>
2025-06-16 09:07:45 -0500

    Scroll sidebar on overflow

--- src/lib/components/publications/Publication.svelte
+++ src/lib/components/publications/Publication.svelte
@@ -162,7 +162,7 @@
 <!-- Table of contents -->
 {#if publicationType !== 'blog' || !isLeaf}
   {#if $publicationColumnVisibility.toc}
-    <Sidebar class='sidebar-leather left-0 md:!pr-8'>
+    <Sidebar class='sidebar-leather left-0 md:!pr-8 overflow-y-auto'>
       <CloseButton onclick={closeToc} class='btn-leather absolute top-0 right-0' />
       <TableOfContents
         displayMode='sidebar'
