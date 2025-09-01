cefde54128b7fa1b0f7dd9886a696e231fa6f4ce
buttercat1791 <mjjurkoic@gmail.com>
2025-09-01 11:27:06 -0500

    Use JsString for relay handle URL

--- lib/relay_selector/src/relay/handle.rs
+++ lib/relay_selector/src/relay/handle.rs
@@ -1,5 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 
 use crate::relay_selector;
@@ -7,7 +8,7 @@ use crate::relay_selector;
 #[wasm_bindgen]
 pub struct RelayHandle {
     #[wasm_bindgen(getter_with_clone)]
-    pub url: String,
+    pub url: js_sys::JsString,
     selector: *const RefCell<relay_selector::RelaySelector>,
 }
 
@@ -15,7 +16,7 @@ impl RelayHandle {
     pub fn new(url: String, selector: &Rc<RefCell<relay_selector::RelaySelector>>) -> Self {
         let selector_clone = Rc::clone(selector);
         RelayHandle {
-            url,
+            url: JsString::from(url),
             selector: Rc::into_raw(selector_clone),
         }
     }
