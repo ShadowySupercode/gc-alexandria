f0c40bc9eed8922b87cf5061de3210f8508ebe01
Nuša Pukšič <nusa.puksic@gmail.com>
2025-09-06 13:06:35 +0200

    Compose

--- src/lib/a/primitives/AAlert.svelte
+++ src/lib/a/primitives/AAlert.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
   import { Alert } from "flowbite-svelte";
 
-  let { color, dismissable, children, title } = $props<{
+  let { color, dismissable, children, title, classes } = $props<{
     color?: string;
     dismissable?: boolean;
     children?: any;
     title?: any;
+    classes?: string;
   }>();
 </script>
 
-<Alert {color} {dismissable} class="alert-leather mb-4">
+<Alert {color} {dismissable} class="alert-leather mb-4 {classes}">
   {#if title}
   <div class="flex">
     <span class="text-lg font-medium">{@render title()}</span>
