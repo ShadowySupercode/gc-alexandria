f3d76dd8dee9f910fae9535d81d4bf4f913b434c
buttercat1791 <mjjurkoic@gmail.com>
2025-09-12 09:25:44 -0500

    Add alias in Svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,7 @@ const config = {
       $lib: "src/lib",
       $components: "src/lib/components",
       $cards: "src/lib/cards",
+      $relay_selector: "lib/relay_selector/pkg/bundler",
     },
   },
 };
